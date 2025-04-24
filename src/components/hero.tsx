8fb55b5fdb416ab983c25e5c2649dfc3d6fa93f4
sarahpratt <smhpratt@gmail.com>
2025-04-24 12:43:03 -0700

    add new group photo

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -42,7 +42,6 @@ export default function HeroHome() {
               ></div>
             </div>
           </div>
-
           {/* Hero image */}
           <div className="text-center">
             <Carousel autoplay>
